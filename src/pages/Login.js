5e19b7021e9236218f6212be7878a906fba08efc
syed.musfar <syed.musfer@convexinteractive.com>
2024-08-22 12:53:54 +0500

    Frontend Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,29 @@
 import { Button, Container, Paper, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Login = () => {
+    const[islogin , setlogin] = useState(true)
   return (
-    <Container maxWidth = 'sx'>
-        <Paper sx={{
+    <Container maxWidth = 'sx' >
+        <Paper
+        
+        elevation={3}
+        sx={{
+            
+            marginTop:'250px',
             padding : 4,
             display:'flex',
             flexDirection:'column',
             alignItems:'center',
-            justifyContent : 'center'
+            justifyContent : 'center',
+            
             
         }}>
+
+
+            {islogin === true ?    
+            <>
             <Typography variant='h4'> Login </Typography>
             <form>
                 <TextField
@@ -33,10 +44,16 @@ const Login = () => {
                 fullWidth
                 />
 
-                <Button variant='contained'type='submit' size='large'  fullWidth > Login </Button>
-                <Button variant='contained'type='submit' size='large'  fullWidth > Register </Button>
+                <Button variant='contained'type='submit' size='large'  fullWidth  > Login </Button>
+                <Button variant='contained'type='submit' size='large'  fullWidth onClick={ () => setlogin(!islogin)} > Register </Button>
 
             </form>
+            </>
+            :
+            <Typography variant='h4'> Register </Typography>
+
+            
+            }
         </Paper>
     </Container>
   )
